c1716381cb3394a142f73f99a98e9d665ba4acb6
Jakub K <karolakjakub90@gmail.com>
2019-07-12 17:01:11 +0200

    firebase wip, adding config file etc

--- src/components/quizGenerator/temp.js
+++ src/components/quizGenerator/temp.js
@@ -1,32 +1,17 @@
 import React, { useState } from 'react'
 import AnswerInput from './AnswerInput'
-import AddAnswerButton from './AddAnswerButton';
-import { DeleteQuestionButton } from './DeleteQuestionButton';
-import * as QuizService from '../services/quizService'
-import { Button } from '@material-ui/core'
-import firebase from 'firebase'
-import * as FirebaseApp from '../../firebase'
 
-
-
-export let questionsArrBackup = [
+export let answersArr = [
     { id: 1, answer: 'aaa', isCorrect: false },
     { id: 2, answer: 'bbb', isCorrect: true },
     { id: 3, answer: 'ccc', isCorrect: false },
 
 ]
 
-firebase.initializeApp()
-const database = firebase.database()
+export default function AnswersList() {
 
-const questionsArr = questionsArrBackup
-console.log(questionsArr)
+    const [answers, setAnswers] = useState(answersArr);
 
-
-export default function AnswersList(props) {
-
-
-    const [answers, setAnswers] = useState(questionsArr) // musi byc state!!!
 
     console.log(answers)
 
@@ -42,51 +27,33 @@ export default function AnswersList(props) {
     }
 
     function onCheckboxChange(state, answerId) {
-        debugger
-        answers.map((answer, index) => {
-
+        setAnswers(answers.map((answer, index) => {
             if (answer.id === answerId) {
 
-
-                return setAnswers({ ...answer, isCorrect: state })
+                return { ...answer, isCorrect: state }
             } else {
-                debugger
                 console.log(answers)
                 return answer
 
             }
-        })
+        }))
     }
 
     function onAnswerDelete(answerId) {
 
+
         setAnswers(answers.filter((answer) => answer.id !== answerId))
-
-    }
-
-    function onAnswerAdd() {
-
-        setAnswers(
-
-            answers.push({ id: 3, answer: 'ccc', isCorrect: false })
-
-        )
-    }
-
-    function onQuizSave() {
-        QuizService.SaveQuiz(questionsArrBackup)
     }
 
     return (
         <div className="quizAnswerInputs">
-            <AddAnswerButton onAnswerAdd={onAnswerAdd}></AddAnswerButton>
 
             {answers.map((answer, index) => {
                 return <AnswerInput
                     key={answer.id}
                     answerIdToShow={index + 1}
                     answerId={answer.id}
-                    answer={answer.answerBody}
+                    answer={answer.answer}
                     onAnswerChange={onAnswerChange}
                     isCorrect={answer.isCorrect}
                     onCheckboxChange={onCheckboxChange}
@@ -94,17 +61,7 @@ export default function AnswersList(props) {
                 ></AnswerInput>
             })}
 
-            <DeleteQuestionButton></DeleteQuestionButton>
-            <br></br>
-            <br></br>
-            <Button
-                onClick={onQuizSave}
-            >Zapisz quiz</Button>
-
         </div>
     )
 }
-
-
-
 
