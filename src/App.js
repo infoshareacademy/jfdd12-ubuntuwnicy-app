74e5d711ff5ace8934f894363b4574e449f336f6
Marta Kirschenstein <kirschenstein.marta@gmail.com>
2019-07-14 20:02:11 +0200

    routing fixed, adding mediaqueries

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+
+function App() {
+  return <div className={'titlePage'}>
+        <h1>sQuizYou</h1>
+        <h2>Witamy!</h2>
+        <div className="infoOnMainPage">
+            <p>Przejdź do zakładki <b>STWÓRZ QUIZ</b>, aby dodać nowy quiz</p>
+            <p>lub</p>
+            <p>Przejdź do zakładki <b>DOŁĄCZ DO QUIZU</b>, aby rozwiązać już istniejący quiz</p>
+      </div>
+    </div>
+};
+
+export default App;
