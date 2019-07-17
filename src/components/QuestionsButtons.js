7efb6c0154a612a0c29a828c84b7c4ac003f4485
Lucjan Kepinski <lucjan.kepinski@gmail.com>
2019-07-17 15:56:11 +0200

    reorganizing files, useless files deleted

--- src/components/QuestionsButtons.js
+++ src/components/QuestionsButtons.js
@@ -1,15 +1,17 @@
 import React from "react";
 import styles from "./QuizTitle.module.css";
-import { QButton } from "./QButton";
 
 const LIMIT = 3;
 
-const Button = ({
-  children,
-  disabled = false,
-  onClick,
-  isQuestionNumber
-}) => <button onClick={onClick} disabled={disabled} className={`${styles.buttonQuestion} ${styles.buttonQuestionNumber}`}>{children}</button>;
+const Button = ({ children, disabled = false, onClick, isQuestionNumber }) => (
+  <button
+    onClick={onClick}
+    disabled={disabled}
+    className={`${styles.buttonQuestion} ${styles.buttonQuestionNumber}`}
+  >
+    {children}
+  </button>
+);
 
 export class QuestionsButtons extends React.Component {
   render() {
@@ -20,33 +22,50 @@ export class QuestionsButtons extends React.Component {
     } = this.props;
 
     const lowerLimit = Math.max(0, currentQuestionId - LIMIT);
-    const upperLimit = Math.min(currentQuestionId + LIMIT, totalNumberOfQuestion - 1);
+    const upperLimit = Math.min(
+      currentQuestionId + LIMIT,
+      totalNumberOfQuestion - 1
+    );
 
-    const lowerButtons = [...Array(currentQuestionId - lowerLimit)].map((_, index) => {
-      const id = currentQuestionId - (currentQuestionId - lowerLimit) + index;
-      return <Button onClick={() => onQuestionChangeHandler(id)}>{id + 1}</Button>;
-    });
+    const lowerButtons = [...Array(currentQuestionId - lowerLimit)].map(
+      (_, index) => {
+        const id = currentQuestionId - (currentQuestionId - lowerLimit) + index;
+        return (
+          <Button onClick={() => onQuestionChangeHandler(id)}>{id + 1}</Button>
+        );
+      }
+    );
 
-    const upperButtons = [...Array(upperLimit - currentQuestionId)].map((_, index) => {
-      const id = currentQuestionId + index + 1;
-      return <Button onClick={() => onQuestionChangeHandler(id)}>{id + 1}</Button>;
-    });
+    const upperButtons = [...Array(upperLimit - currentQuestionId)].map(
+      (_, index) => {
+        const id = currentQuestionId + index + 1;
+        return (
+          <Button onClick={() => onQuestionChangeHandler(id)}>{id + 1}</Button>
+        );
+      }
+    );
 
     return (
       <div>
-        {(
-          <Button disabled={!currentQuestionId} onClick={() => onQuestionChangeHandler(currentQuestionId - 1)}>
+        {
+          <Button
+            disabled={!currentQuestionId}
+            onClick={() => onQuestionChangeHandler(currentQuestionId - 1)}
+          >
             Poprzednie pytanie
           </Button>
-        )}
-        { lowerButtons }
+        }
+        {lowerButtons}
         <Button disabled>{currentQuestionId + 1}</Button>
-        { upperButtons }
-        {(
-          <Button disabled={currentQuestionId === (totalNumberOfQuestion - 1)} onClick={() => onQuestionChangeHandler(currentQuestionId + 1)}>
+        {upperButtons}
+        {
+          <Button
+            disabled={currentQuestionId === totalNumberOfQuestion - 1}
+            onClick={() => onQuestionChangeHandler(currentQuestionId + 1)}
+          >
             Następne pytanie
           </Button>
-        )}
+        }
       </div>
     );
   }
