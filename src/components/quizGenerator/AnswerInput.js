f1bf4d2d75a208eaab8bc4ba2b1fa9a56461903a
Marta Kirschenstein <kirschenstein.marta@gmail.com>
2019-07-22 16:57:56 +0200

    styling WIP

--- src/components/quizGenerator/AnswerInput.js
+++ src/components/quizGenerator/AnswerInput.js
@@ -1,20 +1,9 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import Checkbox from '@material-ui/core/Checkbox';
-import { makeStyles } from '@material-ui/core/styles';
-import IconButton from '@material-ui/core/IconButton';
 
-const useStyles = makeStyles(theme => ({
-    button: {
-        margin: theme.spacing(1),
-    },
-    input: {
-        display: 'none',
-    },
-}));
 
 export default function AnswerInput(props) {
-    const classes = useStyles();
 
     const { answer, isCorrect, answerId, onCheckboxChange, name, onAnswerChange, onClickRemoveAnswer } = props
 
@@ -45,9 +34,9 @@ export default function AnswerInput(props) {
                 name={name}
 
             />
-            <IconButton className={classes.button} aria-label="Delete" name={answerId} onClick={onClickRemoveAnswer} >
+            <button className='removeAnswerButton' name={answerId} onClick={onClickRemoveAnswer} >
                 X
-            </IconButton>
+            </button>
       
         </div>
     )
