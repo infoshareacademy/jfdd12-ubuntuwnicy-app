4a72fce54a4951d4864f8fdab6f2e783ffbf24f0
Marta Kirschenstein <kirschenstein.marta@gmail.com>
2019-07-26 16:54:58 +0200

    WIP

--- src/components/quizGenerator/RemoveQuestionButton.js
+++ src/components/quizGenerator/RemoveQuestionButton.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
 import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import DeleteIcon from '@material-ui/icons/Delete';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import KeyboardVoiceIcon from '@material-ui/icons/KeyboardVoice';
+import Icon from '@material-ui/core/Icon';
+import SaveIcon from '@material-ui/icons/Save';
 
 const useStyles = makeStyles(theme => ({
   button: {
     margin: theme.spacing(1),
   },
-  input: {
-    display: 'none',
+  leftIcon: {
+    marginRight: theme.spacing(1),
+  },
+  rightIcon: {
+    marginLeft: theme.spacing(1),
+  },
+  iconSmall: {
+    fontSize: 20,
   },
 }));
 
@@ -15,7 +27,7 @@ const RemoveQuestionButton = (props) => {
   const classes = useStyles();
 return (
   <div className='removeQuestionButton'>
-    <Button variant="contained" color="secondary" className={classes.button} onClick={props.onClick}>USUŃ PYTANIE</Button>
+    <Button variant="contained" color="secondary" className={classes.button} onClick={props.onClick}>USUŃ PYTANIE <DeleteIcon className={classes.rightIcon} /></Button>
   </div>
 )}
 
