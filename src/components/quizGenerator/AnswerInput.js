9d786ebee929b8a298f1e6681d76e4daea61e9f1
Jakub Karolak <karolakjakub90@gmail.com>
2019-07-29 08:15:45 +0200

    wip

--- src/components/quizGenerator/AnswerInput.js
+++ src/components/quizGenerator/AnswerInput.js
@@ -4,52 +4,52 @@ import Checkbox from '@material-ui/core/Checkbox';
 import { makeStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
-
+import { Button } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
     button: {
-      margin: theme.spacing(1),
+        margin: theme.spacing(1),
     },
     input: {
-      display: 'none',
+        display: 'none',
     },
-  }));
+}));
 
 
 const AnswerInput = (props) => {
     const classes = useStyles();
 
-   const { answer, isCorrect, answerId, onCheckboxChange, name, onAnswerChange, onClickRemoveAnswer } = props
-   return (
-       <div className="answerInputsStyles">
-           <TextField
-               className="answerInput"
-               value={answer}
-               placeholder='Wprowadź odpowiedź'
-               onChange={onAnswerChange}
-               label="Odpowiedź"
-               multiline
-               variant="outlined"
-               answerid={answerId}
-               name={answerId}
-           ></TextField>
-           <Checkbox
-               className="isAnswerCorrect"
-               color="default"
-               iscorrect={'isCorrect'}
-               onClick={onCheckboxChange}
-               answerid={answerId}
-               inputProps={{
-                   'aria-label': 'checkbox with default color',
-               }}
-               checked={isCorrect}
-               name={name}
-           />
-           <IconButton className={classes.button} aria-label="delete" name={answerId} onClick={onClickRemoveAnswer} >
-               <DeleteIcon  name={answerId} onClick={onClickRemoveAnswer}/>
-           </IconButton>
-       </div>
-   )
+    const { answer, isCorrect, answerId, onCheckboxChange, name, onAnswerChange, onClickRemoveAnswer } = props
+    return (
+        <div className="answerInputsStyles">
+            <TextField
+                className="answerInput"
+                value={answer}
+                placeholder='Wprowadź odpowiedź'
+                onChange={onAnswerChange}
+                label="Odpowiedź"
+                multiline
+                variant="outlined"
+                answerid={answerId}
+                name={answerId}
+            ></TextField>
+            <Checkbox
+                className="isAnswerCorrect"
+                color="default"
+                iscorrect={'isCorrect'}
+                onClick={onCheckboxChange}
+                answerid={answerId}
+                inputProps={{
+                    'aria-label': 'checkbox with default color',
+                }}
+                checked={isCorrect}
+                name={name}
+            />
+            <IconButton className={classes.button} aria-label="delete" name={answerId} onClick={onClickRemoveAnswer} >
+               <DeleteIcon name={answerId}></DeleteIcon>
+            </IconButton>
+        </div>
+    )
 }
 
 export default AnswerInput
